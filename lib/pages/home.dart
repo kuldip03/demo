94b673716830b0a9d530277b7f7246a70885a7fc
kanak <kuldipsmart@gmail.com>
2022-07-11 21:20:14 +0530

    day2

---
+++ lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  final int day = 30;
+  final String name = 'Kuldip';
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello'),
+        ),
+        body: Center(child: Text("Welcome To $day days of flutter $name")),
+        drawer: Drawer(),
+      ),
+    );
+  }
+}
